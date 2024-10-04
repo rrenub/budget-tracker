5b97073891adf3593c7b7e734d3017d3fc6d2657
Ruben Delgado <cuenta.rubendelgado@gmail.com>
2024-10-04 16:56:02 +0100

    added basic validation for submitting data

--- api/src/main/java/dev/rdelgado/budgettracker/api/budgetTrackerAPI/dtos/CategoryDto.java
+++ api/src/main/java/dev/rdelgado/budgettracker/api/budgetTrackerAPI/dtos/CategoryDto.java
@@ -1,6 +1,10 @@
 package dev.rdelgado.budgettracker.api.budgetTrackerAPI.dtos;
 
+import dev.rdelgado.budgettracker.api.budgetTrackerAPI.annotations.ValueOfEnum;
 import dev.rdelgado.budgettracker.api.budgetTrackerAPI.enums.MovementType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @AllArgsConstructor
@@ -8,7 +12,12 @@ import lombok.*;
 @Data
 @ToString
 public class CategoryDto {
+
     private Long id;
+
+    @NotBlank(message = "Invalid category name")
     private String name;
+
+    @ValueOfEnum(enumClass = MovementType.class, message = "Invalid type of movement")
     private String type;
 }
--- api/src/main/java/dev/rdelgado/budgettracker/api/budgetTrackerAPI/dtos/MovementDto.java
+++ api/src/main/java/dev/rdelgado/budgettracker/api/budgetTrackerAPI/dtos/MovementDto.java
@@ -1,6 +1,8 @@
 package dev.rdelgado.budgettracker.api.budgetTrackerAPI.dtos;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.sql.Date;
@@ -13,8 +15,15 @@ import java.sql.Date;
 public class MovementDto {
 
     private Long id;
+
+    @NotBlank
     private String concept;
+
+    @NotNull
     private Float amount;
+
+    @NotNull
     private Long categoryId; // Relation to category (ManyToOne)
+
     private String categoryName;
 }
